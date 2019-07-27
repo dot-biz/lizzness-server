2584acfaff88be0c062722e1c0d7cff2545507e1
Alexander Shuping <alexandershuping@gmail.com>
2019-07-27 17:55:37 -0400

    Minor fix

--- Server.gd
+++ Server.gd
@@ -3,6 +3,8 @@ extends Node
 var server: WebSocketServer
 var clients: Node
 
+const LISTEN_PORT = 12926
+
 func _ready():
 	
 	clients = Node.new()
@@ -10,10 +12,12 @@ func _ready():
 	get_tree().get_root().call_deferred("add_child", clients)
 	
 	server = WebSocketServer.new()
-	server.listen(12926, PoolStringArray(), true)
+	server.listen(LISTEN_PORT, PoolStringArray(), true)
 	get_tree().set_network_peer(server)
 	get_tree().connect("network_peer_connected", self, "client_connected")
 	get_tree().connect("network_peer_disconnected", self, "client_disconnected")
+	
+	print('Listening on port %s.' % LISTEN_PORT)
 
 func client_connected(id: int):
 	var client = load("res://Client.tscn").instance()
